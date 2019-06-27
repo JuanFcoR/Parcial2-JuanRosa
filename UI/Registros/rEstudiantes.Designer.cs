da33fdfdbeedbed329984796536ddd8a81ae5705
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2019-06-26 22:52:20 -0400

    Creando la ventana Estudiantes

---
+++ UI/Registros/rEstudiantes.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Parcial2_JuanRosa.UI.Registros
+{
+    partial class rEstudiantes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BalanceNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.EstudainteIdNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.NombresTextBox = new System.Windows.Forms.TextBox();
+            this.EliminarButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.NuevoButton = new System.Windows.Forms.Button();
+            this.GuardarButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.BalanceNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EstudainteIdNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label3.Location = new System.Drawing.Point(14, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Nombres";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.Location = new System.Drawing.Point(14, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Balance";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(14, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "EstudianteId";
+            // 
+            // BalanceNumericUpDown
+            // 
+            this.BalanceNumericUpDown.Location = new System.Drawing.Point(153, 161);
+            this.BalanceNumericUpDown.Name = "BalanceNumericUpDown";
+            this.BalanceNumericUpDown.Size = new System.Drawing.Size(126, 20);
+            this.BalanceNumericUpDown.TabIndex = 12;
+            // 
+            // EstudainteIdNumericUpDown
+            // 
+            this.EstudainteIdNumericUpDown.Location = new System.Drawing.Point(153, 34);
+            this.EstudainteIdNumericUpDown.Name = "EstudainteIdNumericUpDown";
+            this.EstudainteIdNumericUpDown.Size = new System.Drawing.Size(77, 20);
+            this.EstudainteIdNumericUpDown.TabIndex = 11;
+            // 
+            // NombresTextBox
+            // 
+            this.NombresTextBox.Location = new System.Drawing.Point(153, 113);
+            this.NombresTextBox.Name = "NombresTextBox";
+            this.NombresTextBox.Size = new System.Drawing.Size(198, 20);
+            this.NombresTextBox.TabIndex = 10;
+            // 
+            // EliminarButton
+            // 
+            this.EliminarButton.Image = global::Parcial2_JuanRosa.Properties.Resources.borrar;
+            this.EliminarButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.EliminarButton.Location = new System.Drawing.Point(294, 212);
+            this.EliminarButton.Name = "EliminarButton";
+            this.EliminarButton.Size = new System.Drawing.Size(57, 51);
+            this.EliminarButton.TabIndex = 19;
+            this.EliminarButton.Text = "Eliminar";
+            this.EliminarButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.EliminarButton.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::Parcial2_JuanRosa.Properties.Resources.Save_37110;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.Location = new System.Drawing.Point(222, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 51);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Guardar";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // NuevoButton
+            // 
+            this.NuevoButton.Image = global::Parcial2_JuanRosa.Properties.Resources.new_page_document_16676__1_;
+            this.NuevoButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.NuevoButton.Location = new System.Drawing.Point(145, 212);
+            this.NuevoButton.Name = "NuevoButton";
+            this.NuevoButton.Size = new System.Drawing.Size(57, 51);
+            this.NuevoButton.TabIndex = 17;
+            this.NuevoButton.Text = "Nuevo";
+            this.NuevoButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.NuevoButton.UseVisualStyleBackColor = true;
+            // 
+            // GuardarButton
+            // 
+            this.GuardarButton.Image = global::Parcial2_JuanRosa.Properties.Resources.buscar__1_;
+            this.GuardarButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.GuardarButton.Location = new System.Drawing.Point(277, 26);
+            this.GuardarButton.Name = "GuardarButton";
+            this.GuardarButton.Size = new System.Drawing.Size(74, 33);
+            this.GuardarButton.TabIndex = 16;
+            this.GuardarButton.Text = "Buscar";
+            this.GuardarButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.GuardarButton.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label4.Location = new System.Drawing.Point(14, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 17);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Fecha de Ingreso";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(153, 71);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(97, 20);
+            this.dateTimePicker1.TabIndex = 21;
+            // 
+            // rEstudiantes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 281);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.EliminarButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.NuevoButton);
+            this.Controls.Add(this.GuardarButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BalanceNumericUpDown);
+            this.Controls.Add(this.EstudainteIdNumericUpDown);
+            this.Controls.Add(this.NombresTextBox);
+            this.Name = "rEstudiantes";
+            this.Text = "rEstudiantes";
+            ((System.ComponentModel.ISupportInitialize)(this.BalanceNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EstudainteIdNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EliminarButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button NuevoButton;
+        private System.Windows.Forms.Button GuardarButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown BalanceNumericUpDown;
+        private System.Windows.Forms.NumericUpDown EstudainteIdNumericUpDown;
+        private System.Windows.Forms.TextBox NombresTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
