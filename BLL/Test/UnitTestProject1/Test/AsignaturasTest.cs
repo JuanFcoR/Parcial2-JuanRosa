81c4af5a0340e58d7fb160e2263954f2bfc5aefb
Juan Francisco Rosa Cuevas <juan_rosa1@ucne.edu.do>
2019-07-01 17:54:11 -0400

    .

---
+++ BLL/Test/UnitTestProject1/Test/AsignaturasTest.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parcial2_JuanRosa.BLL;
+using Parcial2_JuanRosa.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1.Test
+{
+    [TestClass]
+    public class AsignaturasTest
+    {
+        [TestMethod]
+        public void GuardarTest()
+        {
+            Repositorio<Asignaturas> db = new Repositorio<Asignaturas>();
+            Asignaturas p = new Asignaturas();
+            p.AsignaturaId = 1;
+            p.Descripcion = "Hol";
+            p.Creditos=345;
+
+
+
+            bool paso = false;
+
+            paso = db.Guardar(p);
+
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Repositorio<Asignaturas> repositorio = new Repositorio<Asignaturas>();
+            bool paso = false;
+            Asignaturas p = repositorio.Buscar(1);
+            p.Descripcion = "Asdf";
+            paso = repositorio.Modificar(p);
+            Assert.AreEqual(true, paso);
+
+        }
+
+        [TestMethod()]
+
+        public void BuscarTest()
+        {
+            Repositorio<Asignaturas> repositorio = new Repositorio<Asignaturas>();
+            Assert.IsNotNull(repositorio.Buscar(1));
+
+        }
+
+        [TestMethod()]
+
+        public void GetListTest()
+        {
+            Repositorio<Asignaturas> repositorio = new Repositorio<Asignaturas>();
+            Assert.IsNotNull(repositorio.GetList(p => true));
+
+        }
+
+
+
+        [TestMethod()]
+
+        public void EliminarTest()
+        {
+            Repositorio<Asignaturas> repositorio = new Repositorio<Asignaturas>();
+            Assert.IsTrue(repositorio.Eliminar(1));
+
+        }
+    }
+}
---
+++ BLL/Test/UnitTestProject1/Test/EstudiantesTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parcial2_JuanRosa.BLL;
+using Parcial2_JuanRosa.Entidades;
+
+namespace UnitTestProject1.Test
+{
+    [TestClass]
+    public class EstudiantesTest
+    {
+        [TestMethod]
+        public void GuardarTest()
+        {
+            Repositorio<Estudiantes> db = new Repositorio<Estudiantes>();
+            Estudiantes p = new Estudiantes();
+            p.EstudianteId = 1;
+            p.FechaIngreso = DateTime.Now;
+            p.Nombres = "Juan Rosa";
+
+           
+
+            bool paso = false;
+
+            paso = db.Guardar(p);
+
+            Assert.AreEqual(true, paso);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Repositorio<Estudiantes> repositorio = new Repositorio<Estudiantes>();
+            bool paso = false;
+            Estudiantes p = repositorio.Buscar(1);
+            p.Nombres = "sdfsf";
+            paso = repositorio.Modificar(p);
+            Assert.AreEqual(true, paso);
+
+        }
+
+        [TestMethod()]
+
+        public void BuscarTest()
+        {
+            Repositorio<Estudiantes> repositorio = new Repositorio<Estudiantes>();
+            Assert.IsNotNull(repositorio.Buscar(1));
+
+        }
+
+        [TestMethod()]
+
+        public void GetListTest()
+        {
+            Repositorio<Estudiantes> repositorio = new Repositorio<Estudiantes>();
+            Assert.IsNotNull(repositorio.GetList(p => true));
+
+        }
+
+
+
+        [TestMethod()]
+
+        public void EliminarTest()
+        {
+            Repositorio<Estudiantes> repositorio = new Repositorio<Estudiantes>();
+            Assert.IsTrue(repositorio.Eliminar(1));
+
+        }
+    }
+    
+}
